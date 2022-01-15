6749f1fd53b44bc3f34a65fc5a34e91ecb45b8d8
Ahmad Hamed <Ahmad_Hamed95@yahoo.com>
2022-01-15 21:44:36 +0200

    - Added client-id & client-secret to application.yml - Refactored getUserName end-point in AuthenticationController
    Signed-off-by: Ahmad Hamed <Ahmad_Hamed95@yahoo.com>

--- src/main/java/messaging/controllers/AuthenticationController.java
+++ src/main/java/messaging/controllers/AuthenticationController.java
@@ -2,15 +2,16 @@ package messaging.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class AuthenticationController {
 
   @GetMapping("/user")
-  public String user(@AuthenticationPrincipal OAuth2User principal) {
-    System.out.println(principal);
+  public String getUserName(@AuthenticationPrincipal OAuth2User principal) {
     return principal.getAttribute("name");
   }
 }
